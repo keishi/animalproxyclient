657dea5b51358f0b75337deeb82d50b45133e040
Keishi Hattori <keishi.hattori@gmail.com>
2018-03-26 11:23:48 +0900

    fix

--- animalproxyclient.js
+++ animalproxyclient.js
@@ -1,41 +1,50 @@
+#!/usr/bin/env node
+
 const request = require('request');
 const process = require('process');
 
-const argv = require('yargs').usage('Usage: $0 <animal>').argv;
+const argv = require('yargs')
+    .usage('$0 <animal>', 'start the proxy client', (yargs) => {
+        yargs.positional('animal', {
+            describe: 'the animal id',
+            type: 'string'
+        }).positional('port', {
+            describe: 'the port number to forward the requests to',
+            port: 8081,
+            type: 'number'
+        });
+    }).argv;
 
-if (argv._.length == 0) {
-    console.log(`Must specify an animal`);
-    process.exit(1);
-}
-
-let animal = argv._[0];
+let animal = argv.animal;
+let port = argv.port;
 console.log(`Connecting as ${animal}`);
+console.log(`Forwarding to http://localhost:${port}/action`);
 var socket = require('socket.io-client')(`https://actionproxy.xpeg.org/${animal}`);
-socket.on('connect', function(){
+socket.on('connect', function () {
     console.log(`Connected as ${animal}`);
 });
-socket.on('error', function(err){
+socket.on('error', function (err) {
     console.log(`Error: ${err}`);
     process.exit(1);
 });
-socket.on('action', function(data, cb){
+socket.on('action', function (data, cb) {
     console.log("action query:", data.query);
     request.get({
-        url: "http://localhost:8081/action",
+        url: `http://localhost:${port}/action`,
         qs: {
             q: data.query
         },
         json: true
-      }, (err, res, data) => {
+    }, (err, res, data) => {
         if (err) {
-          console.log('Error:', err);
+            console.log('Error:', err);
         } else if (res.statusCode !== 200) {
-          console.log('Status:', res.statusCode);
+            console.log('Status:', res.statusCode);
         } else {
-          cb(data['textToSpeech']);
+            cb(data['textToSpeech']);
         }
     });
 });
-socket.on('disconnect', function(reason){
+socket.on('disconnect', function (reason) {
     console.log(`Disconnected: ${reason}`);
 });
